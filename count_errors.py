0729289f23cb6755ca4dffcac9a4c2ba18a6f917
Jakob McBroome <jmcbroom@ucsc.edu>
2019-12-14 17:02:46 +0000

    minor bugfix counting N reference bases

--- count_errors.py
+++ count_errors.py
@@ -38,14 +38,15 @@ def main():
             if len(spent) > 4: #ignore empty lines from end of files etc
                 ref = spent[2].upper() #don't particularly care whether its forward or reverse
                 cir = ''.join(c for c in spent[4].upper() if c in 'ACGTN.') #remove characters indicating read start or end or mapping scores or whatever.
-                depth = int(spent[3])
-                assert len(cir) == depth
-                counts[ref] += depth
-                if depth == len(spent[5]): #ignore entries with indels or other complications for this iteration of the pipeline
-                    for i, base in enumerate(cir): #may be length 1.
-                        if base in "ACGT" and base != ref and spent[5][i] in '0123456789':
-                            if int(spent[5][i]) >= args.threshold:
-                                sumerrors[(ref,base)] += 1 
+                if ref != 'N':
+                    depth = int(spent[3])
+                    assert len(cir) == depth
+                    counts[ref] += depth
+                    if depth == len(spent[5]): #ignore entries with indels or other complications for this iteration of the pipeline
+                        for i, base in enumerate(cir): #may be length 1.
+                            if base in "ACGT" and base != ref and spent[5][i] in '0123456789':
+                                if int(spent[5][i]) >= args.threshold:
+                                    sumerrors[(ref,base)] += 1 
 
     print("Error Counts")
     if counts == None:
