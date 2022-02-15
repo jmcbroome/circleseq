b63d57b168e31744c354aca9aed64d1600d1aa4f
Jakob <jmcbroome@gmail.com>
2022-02-15 14:34:46 -0800

    Substantial rewrite for efficiency and better approach to removing duplicate data

--- qc/remove_duplicate_circles.py
+++ qc/remove_duplicate_circles.py
@@ -13,21 +13,16 @@ def argparser():
     parser.add_argument('-i', '--input', help = 'Path to input bam.', required = True)
     parser.add_argument('-o', '--output', help = 'Path to output bam. Default is input with ".dedup" added before the file extension.', default = None)
     parser.add_argument('-s', '--stats', help = 'Save statistics on number of alignments affected and total coverage removed to the target file.', default = None)
+    parser.add_argument('-m', '--min_length', help = 'Set to a minimum value for the length of a consensus sequence to be retained in any case. Default 50', type = int, default = 50)
     args = parser.parse_args()
     return args
-
-def is_encompassed(r1,r2):
-    #we consider r2 to be encompassed by r1 if they have exactly the same length or r2 is shorter
-    #and r2 ends at the same point or earlier
-    #this is a conservative filtering step.
-    return (r1.reference_start <= r2.reference_start and r2.query_length <= r1.query_length and r1.reference_start + r1.query_length >= r2.reference_start + r2.query_length)
 
 def write_stats(reads,outfname,mean_prior,mean_post):
     outf = open(outfname,'w+')
     print("Number of Reads Removed: " + str(len(reads)),file=outf)
     print("Mean Length of Reads Removed: " + str(np.mean([r.query_length for r in reads])),file=outf)
-    print("Mean Coverage Initial: " + str(mean_prior),file=outf)
-    print("Mean Coverage After Removal: " + str(mean_post),file=outf)
+    print("Percentage of Reads Removed: " + str(mean_prior),file=outf)
+    print("Percentage of Coverage Remaining: " + str(mean_post),file=outf)
 
 def main():
     args = argparser()
@@ -38,41 +33,52 @@ def main():
     total_count = 0
     post_covered = 0
     for mc in mapped_chromosomes:
-        #get the set of reads aligned to each base in the chromosome
-        #sort by size of alignment and ask whether each is encompassed by the larger reads in their set
-        #if they are, tag them and ignore them in the future and don't output them
-        #if they aren't, they can be saved to the output
-        for pcol in inputf.pileup(mc):
-            #previously marked reads can also be ignored as candidates for encompasser, since their encompasser will always encompass anything they would encompass
-            #print(dir(pcol.pileups[0].alignment))
-            allreads = pcol.pileups
-            total_covered += len(allreads)
-            total_count += 1
-            reads = sorted([p.alignment for p in allreads if p.alignment not in bad_reads], key=lambda x:x.query_length)
-            icheck = 1
-            removed_here = 0
-            for r in reads:
-                for br in reads[icheck:]:
-                    if is_encompassed(br,r):
-                        bad_reads.add(r)
-                        removed_here += 1
-                        break
-
-                icheck += 1
-            post_covered += len(reads) - removed_here
+        allreads = inputf.fetch(mc)
+        rsd = {}
+        rlens = 0
+        total_reads = 0
+        for r in allreads:
+            if r.query_length < args.min_length:
+                bad_reads.add(r)
+                continue
+            if r.reference_start not in rsd:
+                rsd[r.reference_start] = []
+            rsd[r.reference_start].append(r)
+            rlens += r.query_length
+            total_reads += 1
+        #pick a critical value- assume that the distribution of counts of number of reads which start at a location are normally distributed across the genome
+        #and identify groups of reads that share a start and are unusual...
+        cv = [len(v) for v in rsd.values()]
+        critical = 2*np.std(cv) + np.mean(cv)
+        for p,rc in rsd.items():
+            if len(rc) > critical:
+                #this site is considered a candidate location for pcr duplication
+                #strip out the reads and any reads downstream they contain.
+                #for now, don't even retain one copy of the putative duplicate.
+                maxl = max([r.query_length for r in rc])
+                for i in range(p,p+maxl-args.min_length): 
+                    for r in rsd.get(i,[]):
+                        if i + r.query_length <= p + maxl:
+                            #mark this read for removal.
+                            bad_reads.add(r)
+        total_covered += rlens
+        total_count += total_reads
     if args.output == None:
         outputname = args.input[:-4] + ".dedup.bam"
     else:
         outputname = args.output
     outputf = pysam.AlignmentFile(outputname,'wb',template=inputf)
     #finally, go over it again and write out every read that passed muster.
+    total_removed = 0
     inputf.reset()
     for read in inputf.fetch():
         if read not in bad_reads:
             outputf.write(read)
-
+        else:
+            total_removed += read.query_length
+    post_covered = total_covered - total_removed
     if args.stats != None:
-        write_stats(bad_reads, args.stats, total_covered/total_count, post_covered/total_count)    
+        write_stats(bad_reads, args.stats, len(bad_reads)/total_count*100, post_covered/total_covered*100)    
     inputf.close()
     outputf.close()
 if __name__ == "__main__":
